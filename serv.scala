03d1d89d2b4193381ab010a1e38364af937d9bd1
kadmelia <kadmelia@gmail.com>
2011-05-04 18:04:40 +0200

    File working

--- serv.scala
+++ serv.scala
@@ -3,6 +3,9 @@
 import scala.actors.Actor
 import scala.actors.Actor._
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat
 import java.io._
 import java.net.{InetAddress,ServerSocket,Socket,SocketException}
 import java.io.DataInputStream;
@@ -13,7 +16,14 @@ object server {
   def main(args: Array[String]): Unit = {
     try {
       val listener = new ServerSocket(9999);
-      while (true)
+		
+		var mainDir = new File("photos");
+		
+		if (!mainDir.exists()) {
+			mainDir.mkdir();
+		}
+		
+		while (true)
         new conThread(listener.accept()).start
       listener.close()
     }
@@ -41,7 +51,18 @@ class conThread(sock: Socket) extends Actor {
 		var start = System.currentTimeMillis();
 		
 		var is = sock.getInputStream();
+		
+		var format = new SimpleDateFormat("yyyy-MM-dd");
+		var finalDirName = format.format(new Date);
+		
+		var mainDir = new File("photos" + separator + finalDirName);
+		
+		if (!mainDir.exists()) {
+			mainDir.mkdir();
+		}
+		
 		var fos = new FileOutputStream("test/toto");
+
 		var bos = new BufferedOutputStream(fos);
 		var bytesRead = is.read(mybytearray,0,mybytearray.length);
 		var current = bytesRead;
